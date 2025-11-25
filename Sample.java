d03f095283083aa0458790856b25dc2ce0d550e4
syedafathima223 <sf74309@gmail.com>
2025-11-25 15:33:05 +0530

    mod

--- Sample.java
+++ Sample.java
@@ -1,6 +1,7 @@
-public class main{
+public class Sample{
   public static void main(String[] args){
     System.out.print("Hi");
     System.out.print("Hello");
+    System.out.print("hey");
   }
 }
